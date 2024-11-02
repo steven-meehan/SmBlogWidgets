9b5015ac2c2403445c22f018ca20111974607777
Steven Meehan <steven@stevenmeehan.com>
2024-11-02 14:58:41 -0400

    added the conmponnet that retrieves the configuration and preps and the modal added an environment variable used to set the boolean flag that controls if the modal is displayed to the user

---
+++ projects/news-alert/src/app/news-alert/news-banner/news-banner.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { NewsContent } from '../news-content';
+import { NewsBannerContentComponent } from '../news-banner-content/news-banner-content.component';
+import { NewsBannerContentService } from '../news-banner-content.service';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-news-banner',
+  templateUrl: './news-banner.component.html',
+  styleUrls: ['./news-banner.component.css']
+})
+export class NewsBannerComponent {
+  constructor(
+    private newsBannerContentService: NewsBannerContentService,
+    private modalService: NgbModal = inject(NgbModal)) { 
+    
+    this.subscription
+      .add(this.newsBannerContentService.retrieveContent()
+        .subscribe(data => {
+          console.log("Retrieving News Alerts from the server");
+          let currentDate = new Date();
+          let newsContents = data.filter((item) => item.active && currentDate < new Date(item.validUntil));
+          this.newsContent = newsContents.length >= 1 ? newsContents[0] : null;
+
+          if(this.newsContent && this.newsContent.active && currentDate < new Date(this.newsContent.validUntil)) {
+            if(!sessionStorage.getItem("DoNotDisplayNews")) {
+              this.open();
+            }}
+          },
+          error => { console.log("There was an error retrieving the news alerts from the API"); }
+        ));
+  }
+  
+  subscription = new Subscription();
+  newsContent: NewsContent | null | undefined = null;
+
+  open() {
+    if(environment.setDoNotDhowFlag){
+      sessionStorage.setItem("DoNotDisplayNews", "true");
+    }
+    const modalRef = this.modalService.open(NewsBannerContentComponent);
+    modalRef.componentInstance.newsContent = this.newsContent;
+  }
+}
--- projects/news-alert/src/environments/environment.ts
+++ projects/news-alert/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  configurationBaseUrl: "assets"
+  configurationBaseUrl: "assets",
+  setDoNotDhowFlag: false
 };
 
 /*
